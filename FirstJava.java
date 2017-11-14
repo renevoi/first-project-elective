3aa797a8523891e32a1ffe6131580c9a23dcc999
rene <rene.catapang@gmail.com>
2017-11-14 14:10:42 +0800

    print Jinky Lagrisola

--- FirstJava.java
+++ FirstJava.java
@@ -6,6 +6,8 @@ public class FirstJava {
 
     public static void main(String[] args) {
         System.out.println("First Hello");
+        System.out.println("Jinky Lagrisola");
+        
     }
     
 }
