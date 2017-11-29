fcd7b19e21c8e868253998eef08513a15fbf4558
programmer-employee-1 <programmer.employee.1@gmail.com>
2017-11-29 14:06:18 +0800

    adjusted line spacing

--- PrintAddress.java
+++ PrintAddress.java
@@ -3,9 +3,6 @@ public class PrintAddress {
     public static void main(String[] args) {
         System.out.println("042 Balitoc, Calatagan, Batangas");
       
-        
-        
-        
         System.out.println("Calayo Nasugbu Batangas");
         
         System.out.println("Balitoc Calatagan Batangas");
