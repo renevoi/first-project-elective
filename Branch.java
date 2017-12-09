ee0549a107bb86602acc840e4dd4d289a99e7562
John Charles Villavicencio <31461731+Sofiathe1st@users.noreply.github.com>
2017-12-09 13:48:07 +0800

    Add files via upload

---
+++ Branch.java
@@ -0,0 +1,7 @@
+/*prints name and section(fixed data)*/
+public class Branch {/*Branch is the public class meaning it is accessible entirely on other classes */
+    public static void main(String[] args) {
+        System.out.println("Villavicencio John Charles B.");/*prints the name*/
+        System.out.println("Section: Cpe-4101");/*prints the section*/  
+    }
+}
